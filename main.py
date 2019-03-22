a9ba42f68e6faedb88bc5b2883cdd7b10f7bfe70
Aldo Garcia <aldogarcia54@yahoo.com>
2019-03-22 12:20:01 -0500

    Added FCFS function. Still needs work

--- main.py
+++ main.py
@@ -2,7 +2,42 @@ import sys
 import random
 import math
 
-
+def FCFS():
+    global procsDone
+    global clock
+    global processes
+    global readyQueue
+    turnaround = 0
+    nonUsage = 0
+    readyQueueUsg = 0
+    readyQueue.append(processes[procsDone])
+    while procsDone <= 10001:
+        event = processes[procsDone]
+        if event.arrivalTime < clock:
+            waitTime = clock - event.arrivalTime
+            readyQueueUsg += waitTime
+        elif event.arrivalTime > clock:
+            nonUsage += event.arrivalTime - clock
+        event.completionTime = event.serviceTime + clock
+        clock = event.completionTime
+        # put processes in ready queue
+        t = True
+        i = procsDone + 1
+        if len(readyQueue) > 0:
+            readyQueue = readyQueue[1:]
+        while t:
+            if processes[i].arrivalTime < clock:
+                readyQueue.append(processes[i])
+                i += 1
+            else:
+                t = False
+        processes[procsDone] = event
+        turnaround += event.completionTime - event.arrivalTime
+        procsDone += 1
+    print(turnaround/procsDone)
+    print(procsDone/clock)
+    print(clock-nonUsage)
+    print(readyQueueUsg)
 def generateProcesses():
     procs = []
     servRate = 1 / servTime
@@ -16,15 +51,16 @@ def generateProcesses():
             randNum2 = random.random()
         randArrTime = -math.log(randNum)/arrRate
         time += randArrTime
-        randServTime = math.log(randNum2)/servRate
-        # TODO: create process and add it to list
-        procs.append(time)
+        randServTime = -math.log(randNum2)/servRate
+        proc = Process(i, randServTime, time, randServTime, 0) # TODO: fix completion time variable
+        procs.append(proc)
     return procs
 
 
 #def scheduleEvent(type, time):
     # creates new event and places it in the event queue
     # TODO
+
 
 def processArrival(event):
     global cpuIdle
@@ -40,7 +76,7 @@ def processDeparture(event):
     if readyQueue == []:
         cpuIdle = True
     else:
-        # remove process from ready queue
+        # TODO: remove process from ready queue
         scheduleEvent("DEP", event.time + serviceTime)
 
 class Process:
@@ -58,6 +94,16 @@ class eventNode:
         self.process = process
         self.next = None
 
+    def addNext(self, node):
+        if self.next == None:
+            self.next = node
+        else:
+            self.next.addNext(node)
+
+class eventQueue:
+    def __init__(self):
+        self.head = None
+
 # get input arguments
 for i in range(len(sys.argv)):
     if sys.argv[i] == '-s' and i < (len(sys.argv) - 1):
@@ -72,12 +118,18 @@ for i in range(len(sys.argv)):
 # Initialization
 clock = 0
 processes = generateProcesses()
+'''for process in processes:
+    print(process.completionTime)'''
 procsDone = 0 # end condition (stop when 10,000 processes done)
 readyQueue = []
 cpuIdle = True
-#eventQueue = eventQueue()
+eventQueue = eventQueue()
 
-#
+if scheduler == 1:
+    FCFS()
+    #for proc in processes:
+        #print(proc.completionTime)
+
 '''while procsDone <= 10001:
     event = getEvent()
     clock = event.time
